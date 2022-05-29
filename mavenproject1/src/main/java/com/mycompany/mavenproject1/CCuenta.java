fbafa06758721816e92cdd93d8d5d0ba3e66c831
sergi <sergi@PC-LN1>
2022-05-29 18:04:53 +0100

    V 3.0 del commit de prueba

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/CCuenta.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/CCuenta.java
@@ -1,46 +1,76 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.mavenproject1;
 
 /**
  *
- * @author sergi
+ * @author Sergi
+ * @version v .0 27/05/2022
  */
 public class CCuenta {
 
-
+/**
+  * Nombre del titular de la cuenta
+  */  
     private String nombre;
+    
+  /**
+  * Numero de cuenta
+  */
     private String cuenta;
+    
+  /**
+  *Saldo en cuenta
+  */
     private double saldo;
+    
+  /**
+  *Tipo interés que se va aplicar la cuenta
+  */
     private double tipoInterés;
 
-    public CCuenta()
+    public CCuenta()    // mirar este
     {
     }
-
+    /**
+ * Parámetros del constructor:
+ * @param nom. String que apunta una cuenta hacia una persona.
+ * @param sal. Valor double del saldo en cuenta.
+ * @param tipo. Representa en double el % de interés de que se aplica a la cuenta
+ * @param cue. Es le numero de la cuenta asociada a una persona de tipo String.
+ */
+    
+//metodo 
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
+        tipoInterés=tipo;
     }
-    
-    
+ 
+    /** @return Devuelve saldo de la cuenta
+ */
     public double estado()
     {
         return saldo;
     }
 
-    public void ingresar(double cantidad) throws Exception
-    {
+//METODO QUE AÑADE VALOR A CANTIDAD
+        public void ingresar(double cantidad) throws Exception
+    { 
+        /**
+ * @throws Excepcion. Hace una llamada a Exception y lanzará la excepción 
+ * correspondiente al error encontrado.
+ * 
+ *@param cantidad. Valor tipo double que añade o resta el saldo.
+ */
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
 
+        
+//METODO QUE AÑADE SALDO Y LO RETIRA SIEMPRE Y CUANDO ESTÉ EN POSITIVO.
+        
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -49,15 +79,19 @@ public class CCuenta {
             throw new Exception ("No se hay suficiente saldo");
         saldo = saldo - cantidad;
     }
-
+/** @return Devuelve el nombre al que pertenece una cuenta
+ */
     public String getNombre() {
         return nombre;
     }
 
+    
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    
+/** @return Devuelve la cuenta
+ */
     public String getCuenta() {
         return cuenta;
     }
@@ -66,14 +100,18 @@ public class CCuenta {
         this.cuenta = cuenta;
     }
 
+    /** @return Devuelve el valor saldo
+ */
     public double getSaldo() {
         return saldo;
     }
-
+    
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
-
+    
+/** @return Devuelve el interés al aplicado
+ */
     public double getTipoInterés() {
         return tipoInterés;
     }
@@ -87,9 +125,12 @@ public class CCuenta {
                
         return  "Titular de la cuenta: " + this.nombre + " \n" + "Cuenta: "
                 + this.cuenta + " \n" + "Saldo en cuenta: " + this.saldo + 
-                " \n" + "Tipo de interés: " + this.tipoInterés + " \n" + 
-                "Saldo: "+ this.saldo;
+                " \n" + "Tipo de interés: " + this.tipoInterés;
     }
+    
+    /** @return En este caso retorna el nombre, la cuenta, el saldo y el tipo 
+     * de interés.
+    */
     
 }
  
